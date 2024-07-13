9b033f2dbc2da7ad45433c90a4bddcc1347b195c
sujalcharati <sujalc024@gmail.com>
2024-07-13 17:17:55 +0530

    tasks

--- day1.js
+++ day1.js
@@ -1,1 +1,50 @@
+// task-1
+var num = 45;
+console.log(num);
+
+// task-2
+let name = "javascript";
+console.log(name);
+
+// task-3
+const booleanvalue =true;
+console.log(booleanvalue);
+
+// task-4
+var num = 45;
+console.log(typeof(num));
+
+let naming = "javascript";
+console.log(typeof(naming));
+
+const boolean =true;
+console.log(typeof(boolean)); 
+
+const person ={
+    name:"sujal",
+    profession:"student",
+}
+console.log(typeof(person));
+
+let arrayname = ["js","python",1]
+console.log(typeof(arrayname));
+
+// task-5
+let val  = 3;
+console.log(val);
+val=5
+console.log(val);
+ 
+// task-6
+const vale  = 3;
+console.log(vale);
+vale=5
+console.log(vale);
+
+
+
+
+
+
+
 
