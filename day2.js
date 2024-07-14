3a5b0212708b47dc6244687c4f9db252bd4d798e
sujalcharati <sujalc024@gmail.com>
2024-07-14 20:17:32 +0530

    upto 4 activity

---
+++ day2.js
@@ -0,0 +1,58 @@
+// task1
+a=4;
+b=5;
+console.log(a+b);
+// task2
+a=5;
+b=6;
+console.log(a-b);
+// task3
+a=5;
+b=6;
+console.log(a*b);
+// task4
+a=12;
+b=6;
+console.log(a/b);
+// task5
+a=12;
+b=6;
+console.log(a%b);
+// task6
+a=4
+a+=1;
+console.log(a)
+// task7
+a=4
+a-=1;
+console.log(a)
+// task8
+a=5
+b=6
+if(a>b)
+    console.log("a is greatest");
+else
+console.log("b is greatest");
+// task9
+a=6
+b=6
+if(a>=b)
+console.log("a is greatest or equal to b");
+else
+console.log("b is greatest");
+// task10
+a=true
+b=true
+if(a==b)
+console.log("both have same values");
+else
+console.log("they don't bear same values");
+// task11
+
+
+
+
+
+
+
+
