c18233dc11a9b88d7effbfca5331b3309e61ca9a
sujalcharati <sujalc024@gmail.com>
2024-07-15 16:04:22 +0530

    upto 3 task

---
+++ day3.js
@@ -0,0 +1,35 @@
+// task1
+const num = 10
+if (num > 0) {
+    console.log("The number is positive");
+}
+else if (num < 0) {
+    console.log("The number is negative");
+}
+else {
+    console.log("The number is zero");
+}
+//task2
+age=14
+ if(age>=18)
+ {
+    console.log("the person is eligible to vote");
+ }
+ else{
+    console.log("the person is not eligible to vote");
+
+ }
+//  task3
+a=15;
+b=5;
+c=45;
+ if(a>b & a>c){
+    console.log("a is greatest")
+}
+else if(b>a & b>c){
+    console.log("b is greatest");
+}
+else if (c>a & c>b){
+    
+    console.log("c is greatest");
+}
