da42125776d351454eeea848bfd8f9ab628607d1
sujalcharati <sujalc024@gmail.com>
2024-07-17 10:00:43 +0530

    task4

---
+++ day5.js
@@ -0,0 +1,30 @@
+// task1
+function evenOdd(num){
+    if(num%2==0)
+        console.log("even number");
+    else
+    console.log("odd number");
+}
+const final =evenOdd(7);
+// task2
+ function square (num){
+    return num*num
+}
+const result = square(6);
+console.log(result);
+// task3
+function ismaximum(num1,num2){
+ return Math.max(num1,num2);
+}
+const res = ismaximum(6,8);
+console.log(res);
+// task4
+function stringConcatenate(String1,String2)
+{
+ return String1+" " + String2;
+}
+const ans = stringConcatenate("sujal","charati");
+console.log(ans);
+// task5
+
+
