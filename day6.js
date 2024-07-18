eb7e0ba602997f6d59c01c713eda34d45fb3b2be
sujalcharati <sujalc024@gmail.com>
2024-07-18 09:47:33 +0530

    upto7

---
+++ day6.js
@@ -0,0 +1,29 @@
+// task1
+const arr =[1,2,3,4,5]
+console.log(arr)
+// task2
+const arr1 =[1,2,3,4,5]
+console.log(arr1[0]);
+console.log(arr1[4]);
+// task3
+const arr2 =[1,2,3,4,5]
+arr2.push(6);
+console.log(arr2);
+// task4
+const arr3 =[1,2,3,4,5]
+arr3.pop;
+console.log(arr3);
+// task5
+const arr4 =[1,2,3,4,5]
+arr4.shift(1);
+console.log(arr4);
+// task6
+const arr5 =[1,2,3,4,5]
+arr5.unshift(0);
+console.log(arr5);
+// task7
+const arr6 =[1,2,3,4,5];
+let newarr =arr6.map(arr6*2)
+console.log(newarr);
+
+
