e340939ae770c02561f33a78bf19ee6b84066d46
sujalcharati <sujalc024@gmail.com>
2024-07-22 17:38:15 +0530

    image

---
+++ day10.js
@@ -0,0 +1,10 @@
+// task1
+const button =document.querySelector('#mybutton');
+button.addEventListener("click",addCli);
+function addCli(){
+    
+    document.getElementById("item").innerHTML="after";
+}
+// task2
+
+
