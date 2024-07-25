a86c7083b3830eaaa6e33f9270ca327ca9df1fdd
sujalcharati <sujalc024@gmail.com>
2024-07-25 21:07:16 +0530

    html file

---
+++ day13(m).js
@@ -0,0 +1,5 @@
+import { Sum } from "./day13.js"; 
+const val = Sum();
+console.log(val);
+console.log("hey this is sujal");
+
--- day13.js
+++ day13.js
@@ -2,3 +2,5 @@
 export function Sum(a,b){
 return a+b;
 }
+Sum(1,2);
+// task2 
