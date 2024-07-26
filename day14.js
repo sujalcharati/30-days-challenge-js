8ae536827349dc4eeb0439bc3d8c04355c54d70a
sujalcharati <sujalc024@gmail.com>
2024-07-26 22:51:58 +0530

    file

---
+++ day14.js
@@ -0,0 +1,25 @@
+// task1
+class person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+}
+ getGreeting(){
+    return `hey ${this.name} your age is ${this.age}`;
+ }
+ }
+const person = new person('sujal',20);
+console.log(person.getGreeting());
+
+// task2
+class person1{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+}
+ getGreeting(){
+    return `hey ${this.name} your age is ${this.age}`;
+ }
+ }
+const person = new person('sujal',20);
+console.log(person.getGreeting());
