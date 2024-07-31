3d220dc7e5f3350cb8a2c57d4bec2b1cfe9de12b
sujalcharati <sujalc024@gmail.com>
2024-07-31 21:58:00 +0530

    file

---
+++ day19.js
@@ -0,0 +1,13 @@
+//  task 1
+const str =/javascript/;
+str.test(" hey i have learnt javascript");
+// task 2
+
+let digit="super 30";
+digit.match(/\d/g);
+// task 3
+const text = " Sujal is a name of a Student who is Male.";
+const capitalizedWords = text.match(/\b[A-Z][a-z]*\b/g);
+console.log(capitalizedWords);
+
+// task 4
