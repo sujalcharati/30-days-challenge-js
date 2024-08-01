be46b8411d2ebb649a08530345367532ea02196a
sujalcharati <sujalc024@gmail.com>
2024-08-01 19:41:29 +0530

    file

--- day19.js
+++ day19.js
@@ -11,4 +11,19 @@ const capitalizedWords = text.match(/\b[A-Z][a-z]*\b/g);
 console.log(capitalizedWords);
 
 // task 4
- 
+ const test ="im 20 years old and im thinking of 30 years to have worth of 50";
+ const testsequence =test.match(/\d+/g);
+ console.log(testsequence);
+  
+
+//  task 5
+const areacode ="(123) 456-7890";
+const matching =areacode.match(/(\d{3})\s(\d{3})-(\d{4})/);
+console.log(matching);
+
+// task 6
+const email ="sujac@gmil.com";
+const checkemail = email.match(/\b [A-Z][a-z]*\@ \.\b/g);
+console.log(checkemail);
+
+// task 7
